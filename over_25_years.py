24a00b664a405cc4d80e88b8bac60af7a7b9ab43
EricWithC04 <negativegame117@gmail.com>
2024-05-01 10:43:46 -0300

    feat: calculated ages

--- over_25_years.py
+++ over_25_years.py
@@ -3,19 +3,28 @@ import datetime
 
 def over_25_years():
     ages = pd.read_csv('edades.csv', encoding="utf-8", header=None)
-    formatedAges = []
+    formated_ages = []
 
     today = str(datetime.date.today()).split("-")
 
     for _index, age in ages[1:].iterrows():
-        formatedAges.append(age[0].split("/"))
+        formated_ages.append(age[0].split("/"))
     
-    over_25 = [a for a in formatedAges if int(a[2]) <= 1998]
-    for age in [a for a in formatedAges if int(a[2]) == 1999]:
+    over_25 = [a for a in formated_ages if int(a[2]) <= 1998]
+    for age in [a for a in formated_ages if int(a[2]) == 1999]:
         if (int(age[1]) < 5):
             over_25.append(age)
         elif (int(age[1]) == 5 and int(age[0]) <= 1):
             over_25.append(age)
-    print(over_25)
+    
+    calculated_ages = []
+    for age in over_25:
+        if (int(age[1]) < 5):
+            calculated_ages.append(int(today[0]) - int(age[2]))
+        elif (int(age[1]) == 5 and int(age[0]) <= 1):
+            calculated_ages.append(int(today[0]) - int(age[2]))
+        else:
+            calculated_ages.append(int(today[0]) - int(age[2]) - 1)
+    print(calculated_ages)
 
 over_25_years()
