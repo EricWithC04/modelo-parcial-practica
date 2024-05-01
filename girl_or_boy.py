9c17c85a15da1616d20069c314d2788f4120840e
EricWithC04 <negativegame117@gmail.com>
2024-04-30 22:51:44 -0300

    feat: exceptions

--- girl_or_boy.py
+++ girl_or_boy.py
@@ -1,16 +1,28 @@
 def girl_or_boy(nombre_usuario):
-    chars = []
 
-    for i in nombre_usuario:
-        if i not in chars:
-            chars.append(i)
+    if type(nombre_usuario) != str:
+        raise TypeError("El parametro recibido no es una cadena")
+    
+    if len(nombre_usuario) == 0:
+        raise ValueError("El parametro recibido es una cadena vacia")
+    
+    if nombre_usuario != nombre_usuario.lower():
+        raise ValueError("El parametro recibido debe ser una cadena que contenga solo minusculas")
 
-    if len(chars) % 2 == 0:
-        return "¡ITS A GIRL!"
-    else:
-        return "¡ITS A BOY!"
+    try:
+        chars = []
 
-print(girl_or_boy("EricWithC04"))
+        for i in nombre_usuario:
+            if i not in chars:
+                chars.append(i)
+
+        if len(chars) % 2 == 0:
+            return "¡ITS A GIRL!"
+        else:
+            return "¡ITS A BOY!"
+    except Exception as e:
+        return e
+
 print(girl_or_boy("ericwithc04"))
 print(girl_or_boy("rodriasd"))
 print(girl_or_boy("wiclock"))
