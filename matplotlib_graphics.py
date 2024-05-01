e79bd333a3a642fc0d890255b79b7e9afa71247b
EricWithC04 <negativegame117@gmail.com>
2024-05-01 14:40:56 -0300

    feat: comments in matplotlib_graphics

--- matplotlib_graphics.py
+++ matplotlib_graphics.py
@@ -4,11 +4,18 @@ from over_25_years import over_25_years as o25
 def graphics():
     ages = o25()
 
+    # Creamos un gráfico de lineas y le asignamos las edades como variable independiente y las frecuencias como variable dependiente
     plt.plot(ages["Age"], ages["Frequency"], color="blue")
+
+    # Agregamos un encabezado y los labels de ambos ejes
     plt.title("Frecuencia de cada edad")
     plt.xlabel("Edades")
     plt.ylabel("Frecuencia")
+
+    # Agregamos una cuadricula de fondo
     plt.grid(True)
+
+    # Renderizamos el gráfico
     plt.show()
 
 graphics()
