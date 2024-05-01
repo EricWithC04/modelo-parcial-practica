5102544de06d8c47c69e08f56f190aea745403e4
EricWithC04 <negativegame117@gmail.com>
2024-04-30 23:25:54 -0300

    feat: formated data

--- over_25_years.py
+++ over_25_years.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
 def over_25_years():
-    ages = pd.read_csv('edades.csv', encoding="utf-8")
-    print(ages[0:10])
+    ages = pd.read_csv('edades.csv', encoding="utf-8", header=None)
+    formatedAges = []
+
+    for _index, age in ages[1:].iterrows():
+        formatedAges.append({
+            "date": age[0].split("/"),
+            "name": age[1],
+        })
 
 over_25_years()
