55da1b76bc720f96bbc9d97706609b2f981e59c9
EricWithC04 <negativegame117@gmail.com>
2024-05-01 11:53:55 -0300

    feat: use 'today' and convert all ages in int

--- over_25_years.py
+++ over_25_years.py
@@ -7,24 +7,30 @@ def over_25_years():
 
     today = str(datetime.date.today()).split("-")
 
+    for i, element in enumerate(today):
+        today[i] = int(element)
+
     for _index, age in ages[1:].iterrows():
         formated_ages.append(age[0].split("/"))
     
-    over_25 = [a for a in formated_ages if int(a[2]) <= 1998]
-    for age in [a for a in formated_ages if int(a[2]) == 1999]:
-        if (int(age[1]) < 5):
+    over_25 = [a for a in formated_ages if int(a[2]) < today[0] - 25]
+    for age in [a for a in formated_ages if int(a[2]) == today[0] - 25]:
+        if (int(age[1]) < today[1]):
             over_25.append(age)
-        elif (int(age[1]) == 5 and int(age[0]) <= 1):
+        elif (int(age[1]) == today[1] and int(age[0]) <= today[2]):
             over_25.append(age)
+    
+    for i, element in enumerate(over_25):
+        over_25[i] = [int(element[0]), int(element[1]), int(element[2])]
     
     calculated_ages = []
     for age in over_25:
-        if (int(age[1]) < 5):
-            calculated_ages.append(int(today[0]) - int(age[2]))
-        elif (int(age[1]) == 5 and int(age[0]) <= 1):
-            calculated_ages.append(int(today[0]) - int(age[2]))
+        if (age[1] < today[1]):
+            calculated_ages.append(today[0] - age[2])
+        elif (age[1] == today[1] and age[0] <= today[2]):
+            calculated_ages.append(today[0] - age[2])
         else:
-            calculated_ages.append(int(today[0]) - int(age[2]) - 1)
+            calculated_ages.append(today[0] - age[2] - 1)
     
     calculated_ages.sort()
 
@@ -39,4 +45,4 @@ def over_25_years():
 
     return df
 
-over_25_years()
+print(over_25_years())
